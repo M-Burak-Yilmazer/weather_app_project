793441e2ed80f29b541f5039ebc3722af4b1ff8e
M Burak Yilmazer <mbyilmazer38@gmail.com>
2024-04-03 17:15:05 +0300

    weather detail added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import image from "../src/images/weatherLogo.png";
 import spinner from "../src/images/Vector.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import WeatherDetail from "./components/WeatherDetail";
 
 function App() {
-  const [input, setInput] = useState([]);
+  const [input, setInput] = useState();
   console.log(input);
+  const [weather, setWeather] = useState([]);
+  useEffect(() => {
+    setWeather([]);
+    getWeather();
+  }, [input]);
+  console.log(weather);
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  console.log(API_KEY);
 
+  const getWeather = async () => {
+    if (!input) {
+      return; // input değeri yoksa işlemi sonlandır
+    }
+
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/find?q=${input}&limit=3&units=metric&appid=${API_KEY}&lang=tr`
+      );
+      setWeather(response.data.list);
+    } catch (error) {
+      console.error("Hava durumu alınamadı:", error);
+      // Hata durumunda gerekli işlemleri yapabilirsiniz
+    }
+  };
   return (
     <div className="App">
       <img src={image} width="186px" height="32px" alt="" />
@@ -24,10 +49,16 @@ function App() {
         />
         <img className="spinner" src={spinner} alt="" />
       </div>
-      <ul className="list">
-        <li>istanbul Turkey</li>
-        <li>istanbul Turkey</li>
-       
+      <ul className={`list ${weather.length === 1 ? "single-item" : ""}`}>
+        {input && weather?.length > 0 ? (
+          weather.map((item) => (
+            <WeatherDetail key={item.id} weather={item}>
+              {item.name} - {item.sys.country}
+            </WeatherDetail>
+          ))
+        ) : input ? (
+          <li id="single-item">Weather information not available.</li>
+        ) : null}
       </ul>
     </div>
   );
